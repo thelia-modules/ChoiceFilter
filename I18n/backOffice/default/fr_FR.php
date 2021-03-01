1105ac7568ee96972c46fdcd90f4b5b2fbd7af57
Vincent Lopes-Vicente <vlopes@openstudio.fr>
2021-03-01 14:07:50 +0100

    Fix module to be 2.5 compatible

---
+++ I18n/backOffice/default/fr_FR.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+    'Customize filter configuration for this category' => 'Personnaliser la configuration des filtres pour cette catégorie',
+    'ID' => 'ID',
+    'No' => 'Non',
+    'Position' => 'Position',
+    'Position of the filters in front for this category' => 'Position des filtres en front pour cette category',
+    'Position of the filters in front for this template' => 'Position des filtres en front pour ce gabarit',
+    'Reset default values' => 'Réinitialiser les valeurs par défaut',
+    'Save' => 'Sauvegarder',
+    'The position of the filters is for now the default one.' => 'La position des filtres est pour le moment celle par défaut.',
+    'Title' => 'Titre',
+    'Type' => 'Type',
+    'Visible' => 'Visible',
+    'Yes' => 'Oui',
+);
