f7b7c0518929af646e1ff4298c426a3b800c6b95
Thomas DA SILVA <97163246+ThomasDaSilva@users.noreply.github.com>
2026-06-08 11:51:53 +0200

    Feature Show all filter Category buttons (#3)

--- I18n/backOffice/default/fr_FR.php
+++ I18n/backOffice/default/fr_FR.php
@@ -2,6 +2,7 @@
 
 return array(
     'Customize filter configuration for this category' => 'Personnaliser la configuration des filtres pour cette catégorie',
+    'Hide all (No)' => 'Rendre tout invisible (Non)',
     'ID' => 'ID',
     'No' => 'Non',
     'Position' => 'Position',
@@ -9,6 +10,7 @@ return array(
     'Position of the filters in front for this template' => 'Position des filtres en front pour ce gabarit',
     'Reset default values' => 'Réinitialiser les valeurs par défaut',
     'Save' => 'Sauvegarder',
+    'Show all (Yes)' => 'Rendre tout visible (Oui)',
     'The position of the filters is for now the default one.' => 'La position des filtres est pour le moment celle par défaut.',
     'Title' => 'Titre',
     'Type' => 'Type',
